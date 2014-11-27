44462a94e7ab6fb6b463244685b992ebc489ba9d
BryantDavidDavis <davis.bryant@gmail.com>
2014-11-26 17:48:43 -0800

    Initial Commit

---
+++ c_plus_plus_practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_plus_plus_practice
+//
+//  Created by Bryant Davis on 11/26/14.
+//  Copyright (c) 2014 Bryant Davis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
