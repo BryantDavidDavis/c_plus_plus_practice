63f15160471e46a979d112e974d5632c5fb77c6b
BryantDavidDavis <davis.bryant@gmail.com>
2014-12-05 16:11:12 -0600

    about to build a class

---
+++ c_plus_plus_practice/bry_stack.cpp
@@ -0,0 +1,9 @@
+//
+//  bry_stack.cpp
+//  c_plus_plus_practice
+//
+//  Created by Bryant Davis on 12/2/14.
+//  Copyright (c) 2014 Bryant Davis. All rights reserved.
+//
+
+#include "bry_stack.h"
---
+++ c_plus_plus_practice/bry_stack.h
@@ -0,0 +1,21 @@
+//
+//  bry_stack.h
+//  c_plus_plus_practice
+//
+//  Created by Bryant Davis on 12/2/14.
+//  Copyright (c) 2014 Bryant Davis. All rights reserved.
+//
+
+#ifndef __c_plus_plus_practice__bry_stack__
+#define __c_plus_plus_practice__bry_stack__
+
+#include <stdio.h>
+#include <vector>
+
+
+class Bry_stack {
+private:
+    int top;
+    int size;
+};
+#endif /* defined(__c_plus_plus_practice__bry_stack__) */
